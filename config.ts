4f91e6f1be57e1fa67277a13b0ca508aa14ea5c5
“sayyiku” <liricarain@gmail.com>
2022-04-18 20:44:25 +0800

    :wrench: (update): update
    update

--- config.ts
+++ config.ts
@@ -2,6 +2,16 @@ export const opensource = {
   active: [
     'Sayyiku/FormatCode',
   ],
+  toys: {
+    limit: 3,
+    random: true,
+    repos: [
+      'Sayyiku/FormatCode',
+      'Sayyiku/NormalUtils',
+      'Sayyiku/gatsby-starter-portfolio-cara',
+      'Sayyiku/MoeMarry',
+    ],
+  },
 }
 export const motto = `<p align=center><strong>お可愛いこと。</strong></p>`
 export const github = {
