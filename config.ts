a7117001574a1837cf05fef2132b717508af10e9
“sayyiku” <liricarain@gmail.com>
2022-04-18 19:47:30 +0800

    :fire: (test): test
    1

--- config.ts
+++ config.ts
@@ -1,29 +1,28 @@
-/*
- * @Author: Sayyiku
- * @Date: 2022-04-18 19:11:19
- * @LastEditors: Sayyiku
- * @LastEditTime: 2022-04-18 19:37:55
- * @FilePath: \Innei\config.ts
- * @Description: 
- * 
- * Copyright (c) 2022 by Sayyiku, All Rights Reserved. 
- */
 export const opensource = {
   active: [
-    'Sayyiku/MoeMarry',
-    'Sayyiku/Pixiv-daily-ranking-widget',
+    'mx-space/kami',
+    'mx-space/mx-server',
+    'mx-space/mx-admin',
+    'mx-space/api-client',
   ],
   toys: {
     limit: 3,
     random: true,
     repos: [
-      'Sayyiku/FormatCode',
+      'Innei/react-typewriter',
+      'Innei/simple-clock',
+      'Innei/shuffle-article',
+      'Innei/bump-version',
+      'Innei/candy',
+      'Innei/pageproxy',
+      'Innei/animate-uri',
+      'Innei/rayon-terminal'
     ],
   },
 }
 export const motto = `<p align=center><strong>お可愛いこと。</strong></p>`
 export const github = {
-  name: 'Sayyiku',
+  name: 'Innei',
 }
 
 export const mxSpace = {
